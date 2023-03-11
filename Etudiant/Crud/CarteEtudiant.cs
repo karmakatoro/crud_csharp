4ffeac2be8834fb9fedce26ad139e89b229c852d
karmakatoro <karmakatoro@gmail.com>
2023-03-11 20:38:35 +0200

    start show page

---
+++ Etudiant/Crud/CarteEtudiant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace Crud
+{
+    public partial class CarteEtudiant : DevExpress.XtraReports.UI.XtraReport
+    {
+        public CarteEtudiant()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
